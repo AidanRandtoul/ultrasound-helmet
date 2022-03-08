0c6af7758c9da20fe54195fc9e5e0ccfff146387
2323851r <2323851r@student.gla.ac.uk>
2022-03-08 15:31:58 +0000

    Added skeleton code for arduino

---
+++ sketch_mar08a/sketch_mar08a.ino
@@ -0,0 +1,45 @@
+int interval = 50;
+bool flag = false;
+bool pressed = false;
+const byte inPin = 8;
+
+float testVals[] = {5,4,3,2,1,0.5,0.25};
+int currIndex = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(inPin, INPUT_PULLUP);
+}
+
+void loop () {
+  
+  if (currIndex == 7) {
+    currIndex = 0;
+  }
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(25);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(int(testVals[currIndex]*1000));
+  Serial.print("Current Delay of: ");
+  Serial.println(testVals[currIndex]);
+
+  pressed = false;
+
+  if (digitalRead(inPin) == LOW) {
+    flagSwitch();
+  }
+}
+
+void flagSwitch() {
+  Serial.println("PRESSED!");
+
+  if (!pressed) {
+    flag = !flag;
+    pressed = true;
+    currIndex++;
+  }
+}
